98cb5489750937a9ffd23cec9a443ca7d2d07747
Francisco <franciscogonzalez_hernandez@hotmail.com>
2018-02-26 12:46:02 -0700

    Se versiona avance de detector de rostros, falta que el sevidor responda la peticion

---
+++ android/src/com/udacity/gamedev/gigagal/android/CustomRequest.java
@@ -0,0 +1,10 @@
+package com.udacity.gamedev.gigagal.android;
+
+import com.android.volley.toolbox.StringRequest;
+
+/**
+ * Created by Francisco on 26/02/2018.
+ */
+
+public class CustomRequest extends StringRequest {
+}

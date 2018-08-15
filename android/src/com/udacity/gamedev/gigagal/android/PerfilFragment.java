63cce94f46973cd7379abe81bbb2e21fbec7c207
rockbass2560 <franciscogonzalez_hernandez@hotmail.com>
2018-08-15 00:38:10 -0600

    Se versiona avance con seleccion de fotografia de perfil

--- android/src/com/udacity/gamedev/gigagal/android/PerfilFragment.java
+++ android/src/com/udacity/gamedev/gigagal/android/PerfilFragment.java
@@ -1,12 +1,20 @@
 package com.udacity.gamedev.gigagal.android;
 
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.AppCompatImageButton;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import java.util.Locale;
 
 
 /**
@@ -14,15 +22,38 @@ import android.widget.TextView;
  */
 public class PerfilFragment extends Fragment {
 
-    Persona persona;
+    private Persona persona;
+    private static int REQUEST_GET_SINGLE_FILE = 1;
+    private static int RESULT_OK = -1;
+    private static String TAG = "PerfilFragment";
+    private AppCompatImageButton fotoPerfil;
 
     public PerfilFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode==RESULT_OK){
+            if (requestCode == REQUEST_GET_SINGLE_FILE){
+                String[] projection = {MediaStore.Images.Media.DATA};
+                Cursor cursor = getActivity().getContentResolver().query(data.getData(),projection, null,null, null);
+                if (cursor!=null){
+                    cursor.moveToNext();
+                    String picturePath = cursor.getString(cursor.getColumnIndex(projection[0]));
+                    fotoPerfil.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+                    fotoPerfil.setBackgroundResource(R.color.translucent);
+                    cursor.close();
+                }else
+                    Log.d(TAG, "Cursor vacio");
+            }
+        }
+    }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         Bundle bundle = this.getArguments();
@@ -32,8 +63,18 @@ public class PerfilFragment extends Fragment {
 
         //Se colocan los valores
         ((TextView)fragmentView.findViewById(R.id.name_view)).setText(persona.getName());
-        /*((TextView)fragmentView.findViewById(R.id.text_age)).setText(String.valueOf(persona.getAge()));
-        ((TextView)fragmentView.findViewById(R.id.text_sex)).setText(persona.getSex());*/
+        ((TextView)fragmentView.findViewById(R.id.text_age)).setText(String.format(Locale.getDefault(),"%d %s",persona.getAge(), getResources().getString(R.string.anios)));
+        ((TextView)fragmentView.findViewById(R.id.text_sex)).setText(persona.getSex());
+
+        fotoPerfil = fragmentView.findViewById(R.id.foto_perfil);
+        fotoPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                intent.setType("image/*");
+                startActivityForResult(Intent.createChooser(intent, "Selecciona una imagen"), REQUEST_GET_SINGLE_FILE);
+            }
+        });
 
         return fragmentView;
     }

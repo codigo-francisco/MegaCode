83eec7f019c9fb510150c4853a57f6d26def7aed
rockbass2560 <franciscogonzalez_hernandez@hotmail.com>
2018-09-21 12:48:27 -0600

    Se empiezan modificaciones para cargar los puntajes en fragmento y reutilizarlos en los tab

---
+++ android/src/com/megacode/screens/ScoreFragment.java
@@ -0,0 +1,31 @@
+package com.megacode.screens;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ScoreFragment extends Fragment {
+
+
+    public ScoreFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_score, container, false);
+
+        return view;
+    }
+
+}
---
+++ server/ApiRest/ApiRest/Controllers/ScoreController.cs
@@ -0,0 +1,35 @@
+﻿using ApiRest.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ApiRest.Controllers
+{
+    [Authorize]
+    [RoutePrefix("api/score/")]
+    public class ScoreController : ApiController
+    {
+        megacodeEntities entities = new megacodeEntities();
+
+        [HttpGet]
+        public IHttpActionResult ObtenerScores()
+        {
+            var query = (from u in entities.Usuario
+                         join c in entities.Conexion on u.id equals c.usuarioId
+                         select new
+                         {
+                             Nombre = u.nombre,
+                             Score = u.variables + u.si + u.para + u.mientras,
+                             Dia = c.entrada.Day,
+                             Mes = c.entrada.Month,
+                             Anio = c.entrada.Year
+                         }).OrderByDescending(o => o.Score);
+
+            return Json(query);
+                
+        }
+    }
+}
